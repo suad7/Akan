9085c18689f68159228778c99eb3bd0bbfd3c568
abdulmajid shire <jido@abdulazizs-MacBook-Air.local>
2019-10-25 15:36:31 +0300

    edit color

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 // This will calculate the specific day of the month 
+var century = (year - 1) / 100 + 1;
+
+var dayofweek = Math.floor(((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + date) % 7;
+var theMaleGender = document.getElementById("male");
+var theFemaleGender = document.getElementById("female");
+
+
+
+
 
 
 
