60ee70440db1c966dd973c7b32095a7315b3fb95
abdulmajid shire <jido@abdulazizs-MacBook-Air.local>
2019-10-28 16:57:56 +0300

    edit js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function onCalculate(){
     var maleName = ["Kwasi","Kwadwo"," kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var femaleName = [ "Akosua", "Adwoa","Abenaa"," Akua"," Yaa","Afua","Ama"];
     var dd= parseInt(document.getElementById("day").value);
-    var daysOfTheWeek= ["sunday", "monday" , "tuesday" , "wednesday" , "thursday" , "friday" , "saturday"]
+    var daysOfTheWeek= ["sunday", "monday" , "tuesday" , "wednesday" , "thursday" , "friday" , "saturday"];
     var mm= parseInt(document.getElementById("month").value);
     var yy =parseInt(document.getElementById("year").value);
     var date = new Date(yy + "/" + mm + "/" + dd)
@@ -14,10 +14,10 @@ function onCalculate(){
     //var weekDay = Math.round(( ( (CC/4) - (2 * (CC - 1))) + ((5 * (YY/4)) ) + ((26 * (MM + 1)/10)) + DD ) % 7);
     //if (weekDay === 7){ weekDay = 0; }
     if (male.checked==true) {
-    alert("You were born on " + maleName[results]) +"and you were born on " + daysOfTheWeek[results];
+    alert("You were born on " + maleName[results]) + " and you were born on " + daysOfTheWeek[results];
     }
     else if (female.checked==true){
-    alert("You were born on " + femaleName[results]+ "and you were born on " + daysOfTheWeek[results]);
+    alert("You were born on " + femaleName[results] + " and you were born on " + daysOfTheWeek[results]);
 }
 }
 
